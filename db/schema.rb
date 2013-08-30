46f17c7f6a691c301667078fdc0e04c618dc38d2
Sam Chen <schen7@stevens.edu>
2013-08-30 20:30:42 +0200

    migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -11,52 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130817211147) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "profiles", force: true do |t|
-    t.integer  "user_id"
-    t.string   "ciese_id"
-    t.string   "first_name"
-    t.string   "middle_name"
-    t.string   "last_name"
-    t.string   "prefix"
-    t.string   "title"
-    t.string   "greeting"
-    t.string   "ssn"
-    t.string   "email1"
-    t.string   "email2"
-    t.string   "department"
-    t.string   "subject"
-    t.string   "grade"
-    t.string   "function"
-    t.string   "district"
-    t.string   "affiliation"
-    t.string   "address_line_1"
-    t.string   "address_line_2"
-    t.string   "city"
-    t.string   "state"
-    t.string   "zip"
-    t.string   "country"
-    t.string   "phone1"
-    t.string   "phone2"
-    t.string   "fax"
-    t.string   "home_address_line_1"
-    t.string   "home_address_line_2"
-    t.string   "home_city"
-    t.string   "home_state"
-    t.string   "home_zip"
-    t.string   "home_phone"
-    t.string   "home_mobile"
-    t.string   "home_fax"
-    t.string   "memo1"
-    t.string   "memo2"
-    t.string   "memo3"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20130710192736) do
 
   create_table "users", force: true do |t|
     t.string   "username",        null: false
