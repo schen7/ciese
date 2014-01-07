a1058853a86b26000a50cdeb61e549a2a5e29aed
Jason Sayres <j.c.sayres@gmail.com>
2014-01-07 14:17:49 -0800

    Page render tests

---
+++ spec/requests/render_page_pages_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe "RenderPage" do
+  
+  subject { page }
+
+  describe "view page" do
+    let(:pg) { create(:page, url: "/test", content: "hi there") }
+
+    context "when not published" do
+      it "should not render the page" do
+        visit "/test"
+        expect(page).not_to have_content("hi there")
+        expect(page).to have_content("Not Found")
+      end
+    end
+
+    context "when published" do
+      let!(:published_page) { create(:published_page, version: pg) }
+
+      it "should render the page" do
+        visit "/test"
+        expect(page).to have_content("hi there")
+      end
+    end
+  end
+end
+
