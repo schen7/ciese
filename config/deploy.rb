6fe67b961fed4390b7c24ec852f0e10ec962a058
Jason Sayres <j.c.sayres@gmail.com>
2014-01-23 16:26:10 -0800

    Updated capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,23 +33,36 @@ set :deploy_to, '/var/website/rails/ciese'
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
+# Rbenv settings
+set :rbenv_type, :user
+set :rbenv_ruby, '2.0.0-p247'
+# set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all
+
+
 namespace :deploy do
 
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
-    task command, roles: :app, except: {no_release: true} do
-      run "/etc/init.d/unicorn_#{application} #{command}"
+    task command do
+      on roles(:app) do |host|
+        run "/etc/init.d/unicorn_#{application} #{command}"
+      end
     end
   end
 
-  task :setup_config, roles: :app do
-    # sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-    # sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
-    run "mkdir -p #{shared_path}/config"
-    # put File.read("config/database.yml"), "#{shared_path}/config/database.yml"
-    puts "Now edit the config files in #{shared_path}."
+  desc "Set up config files."
+  task :setup_config do
+    on roles(:app) do |host|
+      # sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+      # sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+      run "mkdir -p #{shared_path}/config"
+      # put File.read("config/database.yml"), "#{shared_path}/config/database.yml"
+      puts "Now edit the config files in #{shared_path}."
+    end
   end
-  after "deploy:setup", "deploy:setup_config"
+  # after "deploy:setup", "deploy:setup_config"
 
   # task :symlink_config, roles: :app do
   #   run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
@@ -57,11 +70,13 @@ namespace :deploy do
   # after "deploy:finalize_update", "deploy;symlink_config"
 
   desc "Make sure local git is in sync with remote."
-  task :check_revision, roles: :web do
-    unless `git rev-parse HEAD` == `git rev-parse origin/master`
-      puts "WARNING: HEAD is not the same as origin/master"
-      puts "Run `git push` to sync changes."
-      exit
+  task :check_revision do
+    on roles(:web) do |host|
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
     end
   end
   before "deploy", "deploy:check_revision"
